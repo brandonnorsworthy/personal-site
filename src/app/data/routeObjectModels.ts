0e1ad6b3a0a28c8dc43fa17f4220449ca619c189
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2024-01-10 19:28:14 -0600

    uncomment models

--- src/app/data/routeObjectModels.ts
+++ src/app/data/routeObjectModels.ts
@@ -5,15 +5,15 @@ export type routeObjectModelTypes = {
 }
 
 const routeObjectModels: routeObjectModelTypes[] = [
-  // {
-  //   name: "laptop",
-  //   path: "laptop.glb",
-  //   tags: ["technology", "computer", "windows", "apple", "programming", "coding", "software", "hardware", "gaming"]
-  // },
-  // {
-  //   name: "car",
-  //   path: "car.glb",
-  // },
+  {
+    name: "laptop",
+    path: "laptop.glb",
+    tags: ["technology", "computer", "windows", "apple", "programming", "coding", "software", "hardware", "gaming"]
+  },
+  {
+    name: "car",
+    path: "car.glb",
+  },
   {
     name: "house",
     path: "house.glb",
