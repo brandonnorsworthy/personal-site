c17d48b8773624680ed75bceb240b4ffc06f2811
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2024-08-03 16:52:31 -0500

    add new link for registry

--- src/app/wedding/registry/page.tsx
+++ src/app/wedding/registry/page.tsx
@@ -8,6 +8,9 @@ import withLayout from "../../hocs/withLayout";
 import cityWalk from "../../../../public/images/CityWalk.png"
 import crateAndBarrelLogo from "../../../../public/images/CrateAndBarrel.png"
 
+import amazonLogo from "../../../../public/images/wedding/amazon_logo.png"
+import amazonImage from "../../../../public/images/wedding/amazon_image.png"
+
 export const metadata = {
   title: 'Gift Registry',
   description: 'Here, you can include details about the location, how to get there, parking information, and other logistics.',
@@ -18,10 +21,11 @@ interface PageProps {
 }
 
 const Page: React.FC<PageProps> = ({ responsiveConstraintClasses }) => {
-  const registry = "https://www.crateandbarrel.com/gift-registry/madison-minton/r6752612";
+  const crateAndBarrelRegistry = "https://www.crateandbarrel.com/gift-registry/madison-minton/r6752612";
+  const amazonRegistry = "https://www.amazon.com/wedding/share/NorsworthyWedding";
 
   return (
-    <main className='min-h-[100vh] flex flex-col items-center w-full'>
+    <main className='min-h-[100vh] flex flex-col items-center w-full overflow-x-hidden'>
       <div className="flex justify-center w-full bg-wedding-primary-highlight">
         <div className={responsiveConstraintClasses}>
           <Navbar />
@@ -34,16 +38,16 @@ const Page: React.FC<PageProps> = ({ responsiveConstraintClasses }) => {
             We have a registry with Crate&Barrel
           </span>
           <Link
-            href={registry}
+            href={crateAndBarrelRegistry}
             target="_blank"
             rel="noopener noreferrer"
             className='underline text-wedding-primary-shadow'>
             click here to view!
           </Link>
         </div>
-        <div className="relative flex justify-center w-full p-8 mt-8">
+        <div className="relative flex justify-center w-full p-8 mt-8 h-[400px]">
           <Link
-            href={registry}
+            href={crateAndBarrelRegistry}
             target="_blank"
             rel="noopener noreferrer">
             <div className='w-[50vw] h-[50vw] md:w-80 md:h-80 border-solid border-8 border-wedding-secondary-shadow absolute rotate-[17deg] z-20 left-[40vw] md:left-[55%] overflow-hidden '>
@@ -59,6 +63,42 @@ const Page: React.FC<PageProps> = ({ responsiveConstraintClasses }) => {
               <Image
                 src={crateAndBarrelLogo}
                 alt='crate and barrel logo'
+                placeholder='blur'
+                width={400}
+                height={400} />
+            </div>
+          </Link>
+        </div>
+
+        <div className="flex flex-col items-center justify-center w-full">
+          <span>
+            We have a Amazon List aswell
+          </span>
+          <Link
+            href={amazonRegistry}
+            target="_blank"
+            rel="noopener noreferrer"
+            className='underline text-wedding-primary-shadow'>
+            click here to view!
+          </Link>
+        </div>
+        <div className="relative flex justify-center w-full p-8 mt-8 h-[400px]">
+          <Link
+            href={amazonRegistry}
+            target="_blank"
+            rel="noopener noreferrer">
+            <div className='w-[50vw] h-[50vw] md:w-80 md:h-80 border-solid border-8 absolute rotate-[12deg] z-10 left-[40vw] md:left-[55%] overflow-hidden '>
+              <Image
+                src={amazonLogo}
+                alt='amazon logo image'
+                placeholder='blur'
+                width={400}
+                height={400} />
+            </div>
+            <div className='w-[50vw] h-[50vw] md:w-80 md:h-80  border-wedding-secondary-shadow border-solid border-8 absolute -rotate-[15deg] z-20 right-[40vw] md:right-[45%] flex justify-center items-center overflow-hidden'>
+              <Image
+                src={amazonImage}
+                alt='picture of brandon and madison'
                 placeholder='blur'
                 width={400}
                 height={400} />
